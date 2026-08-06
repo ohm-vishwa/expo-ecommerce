71f3462535ea897ec82cf388c4172850908250bf
ohm-vishwa <ohmdto1513@gmail.com>
2026-08-06 14:01:18 +0530

    feat: integrate Inngest for user event handling and add user model
    - Added Inngest package to handle user creation and deletion events.
    - Implemented functions to sync new users and delete users from the database.
    - Created a user model with address and wishlist schemas.
    - Updated server configuration to include Inngest middleware for event processing.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,18 @@ import path from "path";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { server } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-console.log(__dirname);
+app.use(express.json());
 
 app.use(clerkMiddleware()); // adds auth object under the request req => req.auth
+
+app.use("/api/inngest", server({ client: inngest, function: functions }));
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
